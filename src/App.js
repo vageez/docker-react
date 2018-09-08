82042f00a0879c01198552e5e05b085a9c0674df
Angelo <avagenas14@gmail.com>
2018-09-08 17:54:35 -0400

    first feature change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Edit me please! <code>src/App.js</code> and save to reload.
+          First feature change! <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
